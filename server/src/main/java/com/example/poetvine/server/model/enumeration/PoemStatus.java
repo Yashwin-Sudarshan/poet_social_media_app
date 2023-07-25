d9d82c46f8c66ae522b25b973f753416bb38d8ee
Yashwin Sudarshan <yashwin.sudarshan@fdmgroup.com>
2023-07-25 21:28:21 +1000

    Restart server from scratch

--- server/src/main/java/com/example/poetvine/server/model/enumeration/PoemStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.poetvine.server.model.enumeration;
-
-public enum PoemStatus {
-    PUBLISHED,
-    DRAFT
-}
--- server/src/main/java/com/example/poetvine/server/model/enumeration/VisibilityPreference.java
+++
@@ -1,7 +0,0 @@
-package com.example.poetvine.server.model.enumeration;
-
-public enum VisibilityPreference {
-    PUBLIC,
-    FOLLOWERS,
-    NONE
-}
