2c3bc26dd18be32a3001bf83e34f790f9253d16c
Yashwin Sudarshan <yashwin.sudarshan@fdmgroup.com>
2023-07-25 22:28:58 +1000

    Setup spring project and postgres docker connection and configuration

---
+++ server/src/test/java/com/example/poetvine/server/PoetvineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poetvine.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoetvineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
