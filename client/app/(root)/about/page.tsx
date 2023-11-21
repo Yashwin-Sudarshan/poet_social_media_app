c02cf3f633549a8a750180ffc597ce9fa614b250
Yashwin Sudarshan <49670540+Yashwin-Sudarshan@users.noreply.github.com>
2023-11-21 13:55:40 +1100

    fix responsive layout for about page

--- client/app/(root)/about/page.tsx
+++ client/app/(root)/about/page.tsx
@@ -2,25 +2,27 @@ import React from "react";
 
 const Page = () => {
   return (
+    <div className="mx-auto max-w-[1440px]">
     <div
       className="mx-auto w-4/5 pb-60 pt-10 min-[540px]:ml-20 min-[540px]:max-w-sm md:max-w-lg
       lg:ml-[138px] lg:max-w-[663px]"
     >
-      <h1 className="mb-10 text-5xl font-bold text-brown dark:text-pale">
+      <h1 className="mb-10 text-5xl font-bold text-brown dark:text-pale
+        max-[430px]:text-2xl">
         About
       </h1>
-      <p className="text-lg text-brown dark:text-pale">
+      <p className="text-lg text-brown dark:text-pale max-[430px]:text-base">
         Welcome to our social media platform for poets!
       </p>
       <br />
-      <p className="text-lg text-brown dark:text-pale">
+      <p className="text-lg text-brown dark:text-pale max-[430px]:text-base">
         At Poetvine, we believe that poetry has the power to connect people,
         inspire emotions, and ignite conversations. Our platform is dedicated to
         providing a vibrant and supportive community where poets of all levels
         can share their work, engage with fellow poets, and find inspiration.
       </p>
       <br />
-      <p className="text-lg text-brown dark:text-pale">
+      <p className="text-lg text-brown dark:text-pale max-[430px]:text-base">
         What sets us apart is our focus on fostering a creative and inclusive
         environment. We celebrate the diversity of poetic styles and voices,
         encouraging poets from various backgrounds and cultures to express
@@ -29,21 +31,21 @@ const Page = () => {
         grow, and connect with like-minded individuals.
       </p>
       <br />
-      <p className="text-lg text-brown dark:text-pale">
+      <p className="text-lg text-brown dark:text-pale max-[430px]:text-base">
         Here, you can showcase your poems, create your own portfolio, and
         receive feedback from a community of passionate poets. Connect with
         fellow poets through comments and likes, forging meaningful connections
         and building friendships along the way.
       </p>
       <br />
-      <p className="text-lg text-brown dark:text-pale">
+      <p className="text-lg text-brown dark:text-pale max-[430px]:text-base">
         We prioritize your privacy and security. Your personal information and
         creative works are protected, allowing you to express yourself without
         worry. We value your freedom of expression and strive to maintain a
         respectful and inclusive environment for all members.
       </p>
       <br />
-      <p className="text-lg text-brown dark:text-pale">
+      <p className="text-lg text-brown dark:text-pale max-[430px]:text-base">
         Join us on this poetic journey and become a part of a vibrant community
         where your words have the power to touch hearts, provoke thoughts, and
         inspire change. Whether you&apos;re seeking inspiration, feedback, or
@@ -51,11 +53,12 @@ const Page = () => {
         support and empower you.
       </p>
       <br />
-      <p className="text-lg text-brown dark:text-pale">
+      <p className="text-lg text-brown dark:text-pale max-[430px]:text-base">
         Unleash your creativity and connect with kindred spirits. Start sharing
         your poetic voice today on Poetvine. Together, let&apos;s create a world
         where poetry flourishes and connects us all.
       </p>
+    </div>
     </div>
   );
 };
